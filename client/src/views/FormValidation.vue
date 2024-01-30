67e2cff5b286f3e743e9e77969c1686196c47049
Cristian Campos <20213tn003@utez.edu.mx>
2024-01-30 10:53:49 -0600

    [ADD] form validaitons

--- client/src/views/FormValidation.vue
+++ client/src/views/FormValidation.vue
@@ -3,7 +3,8 @@
     <b-form>
       <b-row>
         <b-col cols="12" sm="12" md="4">
-          <b-form-group id="name-group" label="Nombre:" label-for="name-input">
+          <b-form-group id="name-group">
+            <label for="name">Nombre:&nbsp;<b class="text-danger">*</b> </label>
             <b-form-input
               id="name-input"
               type="text"
@@ -20,7 +21,9 @@
         </b-col>
         <b-col cols="12" sm="12" md="4">
           <b-form-group id="surname-group">
-            <label for="surname-input">Apellido paterno:</label>
+            <label for="surname-input"
+              >Apellido paterno:&nbsp;<b class="text-danger">*</b>
+            </label>
             <b-form-input
               id="surname-input"
               type="text"
@@ -55,31 +58,38 @@
       </b-row>
       <b-row class="mt-4">
         <b-col cols="12" sm="12" md="4">
-          <b-form-group
-            id="cp-group"
-            label="Código postal:"
-            label-for="cp-input"
-          >
+          <b-form-group id="cp-group">
+            <label for="cp-input"
+              >Cógido postal:&nbsp;<b class="text-danger">*</b></label
+            >
             <b-form-input
               id="cp-input"
               type="text"
               placeholder="Ingresa tu código postal"
               @keypress="signal"
-              min="0"
               maxlength="5"
               v-model="v$.postalCode.$model"
               @blur="v$.postalCode.$touch"
               :state="v$.postalCode.$dirty ? !v$.postalCode.$error : null"
             >
             </b-form-input>
-            <b-form-invalid-feedback v-if="v$.postalCode.$error"
+            <b-form-invalid-feedback v-if="!v$.postalCode.required.$response"
               >Campo obligatorio</b-form-invalid-feedback
+            >
+            <b-form-invalid-feedback
+              v-if="
+                !v$.postalCode.valid.$response &&
+                v$.postalCode.required.$response
+              "
+              >C.P inválido (Mínimo 5 caracteres)</b-form-invalid-feedback
             >
           </b-form-group>
         </b-col>
         <b-col cols="12" sm="12" md="4">
           <b-form-group id="street-group">
-            <label for="street-input">Calle:</label>
+            <label for="street-input"
+              >Calle:&nbsp;<b class="text-danger">*</b></label
+            >
             <b-form-input
               id="street-input"
               type="text"
@@ -96,7 +106,9 @@
         </b-col>
         <b-col cols="12" sm="12" md="4">
           <b-form-group id="number-group">
-            <label for="number-input">Número (#):</label>
+            <label for="number-input"
+              >Número (#):&nbsp;<b class="text-danger">*</b></label
+            >
             <b-form-input
               id="number-input"
               type="text"
@@ -114,7 +126,10 @@
       </b-row>
       <b-row class="mt-4">
         <b-col cols="12" sm="12" md="4">
-          <b-form-group id="city-group" label="Ciudad:" label-for="city-input">
+          <b-form-group id="city-group">
+            <label for="city-input"
+              >Ciudad:&nbsp;<b class="text-danger">*</b></label
+            >
             <b-form-input
               id="city-input"
               type="text"
@@ -131,38 +146,85 @@
         </b-col>
         <b-col cols="12" sm="12" md="4">
           <b-form-group id="birthday-group">
-            <label for="birthday-input">Fecha de nacimiento:</label>
+            <label for="birthday-input"
+              >Fecha de nacimiento:&nbsp;<b class="text-danger">*</b></label
+            >
             <b-form-input
               id="birthday-input"
               type="date"
               placeholder="Ingresa tu fecha de naciemiento"
+              v-model="v$.birthday.$model"
+              @blur="v$.birthday.$touch"
+              :state="v$.birthday.$dirty ? !v$.birthday.$error : null"
             >
             </b-form-input>
+            <b-form-invalid-feedback v-if="!v$.birthday.required.$response"
+              >Campo obligatorio</b-form-invalid-feedback
+            >
+            <b-form-invalid-feedback
+              v-if="
+                !v$.birthday.valid.$response && v$.birthday.required.$response
+              "
+              >Fecha de nacimiento inválida (Debe ser mayor a 18
+              años)</b-form-invalid-feedback
+            >
           </b-form-group>
         </b-col>
         <b-col cols="12" sm="12" md="4">
           <b-form-group id="email-group">
-            <label for="email-input">Correo electrónico:</label>
+            <label for="email-input"
+              >Correo electrónico:&nbsp;<b class="text-danger">*</b></label
+            >
             <b-form-input
               id="email-input"
               type="email"
-              placeholder="Ingresa el número de tu dirección"
+              placeholder="Ingresa tu correo"
               v-model="v$.email.$model"
               @blur="v$.email.$touch"
               :state="v$.email.$dirty ? !v$.email.$error : null"
             >
             </b-form-input>
-            <b-form-invalid-feedback v-if="!v$.email.required"
+            <b-form-invalid-feedback v-if="!v$.email.required.$response"
               >Campo obligatorio</b-form-invalid-feedback
             >
-            <b-form-invalid-feedback v-if="v$.email.email && v$.email.required"
-              >Correo no válido</b-form-invalid-feedback
+            <b-form-invalid-feedback
+              v-if="!v$.email.valid.$response && v$.email.required.$response"
+              >Correo inválido</b-form-invalid-feedback
             >
           </b-form-group>
         </b-col>
-        <b-col cols="12" sm="12" class="mt-4">
+      </b-row>
+      <b-row class="mt-4">
+        <b-col cols="12" sm="12" md="4">
+          <b-form-group id="phone-group">
+            <label for="phone-input"
+              >Teléfono:&nbsp;<b class="text-danger">*</b></label
+            >
+            <b-form-input
+              id="phone-input"
+              type="text"
+              placeholder="Ingresa tu número de teléfono"
+              v-model="v$.phone.$model"
+              @keypress="signal"
+              @blur="v$.phone.$touch"
+              :state="v$.phone.$dirty ? !v$.phone.$error : null"
+              maxlength="10"
+            >
+            </b-form-input>
+            <b-form-invalid-feedback v-if="!v$.phone.required.$response"
+              >Campo obligatorio</b-form-invalid-feedback
+            >
+            <b-form-invalid-feedback
+              v-if="!v$.phone.valid.$response && v$.phone.required.$response"
+              >Teléfono inválido (Min. 10 caracteres)</b-form-invalid-feedback
+            >
+          </b-form-group>
+        </b-col>
+        <b-col cols="12" sm="12" md="8">
           <b-form-group>
-            <label for="email-input">Fotografía:</label>
+            <label for="photo-input"
+              >Fotografía:&nbsp;<b class="text-danger">*</b></label
+            >
             <b-form-file
               multiple
               placeholder="Selecciona un archivo"
@@ -173,9 +235,7 @@
       </b-row>
       <b-row class="mt-4">
         <b-col cols="12" sm="12" class="d-flex flex-row-reverse">
-          <b-button variant="secondary">
-            Enviar
-          </b-button>
+          <b-button variant="secondary"> Enviar </b-button>
         </b-col>
       </b-row>
     </b-form>
@@ -186,11 +246,6 @@
 import Vue from "vue";
 import { useVuelidate } from "@vuelidate/core";
 import { required, email } from "@vuelidate/validators";
-// const signal = (event) => {
-//   if ((event.charCode >= 48 && event.charCode <= 57) || event.charCode == 46)
-//     return true;
-//   else return false;
-// };
 export default Vue.extend({
   name: "FormValidation",
   setup() {
@@ -208,6 +263,7 @@ export default Vue.extend({
       birthday: "",
       email: "",
       photo: "",
+      phone: "",
     };
   },
   methods: {
@@ -226,12 +282,51 @@ export default Vue.extend({
       name: { required },
       surname: { required },
       lastname: {},
-      postalCode: { required },
+      postalCode: {
+        required,
+        valid: function (value) {
+          const isValid = /^[0-9]{5}/.test(value);
+          return isValid;
+        },
+      },
       street: { required },
       number: { required },
       city: { required },
-      birthday: {},
-      email: { required, email },
+      birthday: {
+        required,
+        valid: function (value) {
+          const parts = value.split("-");
+          const year = parseInt(parts[0]);
+          const month = parseInt(parts[1]) - 1;
+          const day = parseInt(parts[2]);
+          const birthdate = new Date(year, month, day);
+          const today = new Date();
+          const minAgeDate = new Date(
+            today.getFullYear() - 18,
+            today.getMonth(),
+            today.getDate()
+          );
+
+          return birthdate <= minAgeDate;
+        },
+      },
+      email: {
+        required,
+        valid: function (value) {
+          const isValid =
+            /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+              value
+            );
+          return isValid;
+        },
+      },
+      phone: {
+        required,
+        valid: function (value) {
+          const isValid = /^[0-9]{10}/.test(value);
+          return isValid;
+        },
+      },
     };
   },
 });
